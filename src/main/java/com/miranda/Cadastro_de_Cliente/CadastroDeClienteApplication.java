36f3a9beba9f713583d408c6bf089dee3affdb47
Adriano Miranda <72226389+adrianommiranda@users.noreply.github.com>
2026-01-29 20:24:31 -0300

    Commit inicial

---
+++ src/main/java/com/miranda/Cadastro_de_Cliente/CadastroDeClienteApplication.java
@@ -0,0 +1,13 @@
+package com.miranda.Cadastro_de_Cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miranda/Cadastro_de_Cliente/CadastroDeClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miranda.Cadastro_de_Cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
